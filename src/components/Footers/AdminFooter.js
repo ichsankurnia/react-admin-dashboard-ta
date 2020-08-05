310b4b2558e48ef37d093c1cc6c98fefc39d9712
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-08-05 17:36:23 +0700

    update href

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -9,9 +9,9 @@ class Footer extends React.Component {
 			<footer className="footer">
 				<Row className="align-items-center justify-content-xl-between">
 					<Col xl="6">
-						<div className="copyright text-center text-xl-left text-muted">© 2018{" "}
-							<a className="font-weight-bold ml-1" href="https://www.creative-tim.com?ref=adr-admin-footer" rel="noopener noreferrer" target="_blank">
-								Creative Tim
+						<div className="copyright text-center text-xl-left text-muted">© 2020{" "}
+							<a className="font-weight-bold ml-1" href="https://goes2nobel.com" rel="noopener noreferrer" target="_blank">
+								Goes2Nobel
 							</a>
 						</div>
 					</Col>
@@ -19,8 +19,8 @@ class Footer extends React.Component {
 					<Col xl="6">
 						<Nav className="nav-footer justify-content-center justify-content-xl-end">
 							<NavItem>
-								<NavLink href="https://www.creative-tim.com?ref=adr-admin-footer" rel="noopener noreferrer" target="_blank">
-									Creative Tim
+								<NavLink href="https://goes2nobel.com" rel="noopener noreferrer" target="_blank">
+									Goes2Nobel
 								</NavLink>
 							</NavItem>
 
