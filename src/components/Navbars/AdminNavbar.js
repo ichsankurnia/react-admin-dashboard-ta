5eac7475781cb8e409f6554742320ed4ce6a8c64
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-08-06 17:59:39 +0700

    get username and avatar from api

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+
 // reactstrap components
 import {
 	DropdownMenu,
@@ -18,70 +19,79 @@ import {
 	Media
 } from "reactstrap";
 
-class AdminNavbar extends React.Component {
-	render() {
-		return (
-			<>
-				<Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
-					<Container fluid>
-						<Link className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" to="/">{this.props.brandText}</Link>
-						<Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-							<FormGroup className="mb-0">
-								<InputGroup className="input-group-alternative">
-									<InputGroupAddon addonType="prepend">
-										<InputGroupText>
-											<i className="fas fa-search" />
-										</InputGroupText>
-									</InputGroupAddon>
-									<Input placeholder="Search" type="text" />
-								</InputGroup>
-							</FormGroup>
-						</Form>
-						<Nav className="align-items-center d-none d-md-flex" navbar>
-							<UncontrolledDropdown nav>
-								<DropdownToggle className="pr-0" nav>
-									<Media className="align-items-center">
-										<span className="avatar avatar-sm rounded-circle">
-											<img alt="..." src={require("./../../assets/img/theme/team-4-800x800.jpg")}/>
-										</span>
-										<Media className="ml-2 d-none d-lg-block">
-											<span className="mb-0 text-sm font-weight-bold">Jessica Jones</span>
-										</Media>
-									</Media>
-								</DropdownToggle>
-									<DropdownMenu className="dropdown-menu-arrow" right>
-										<DropdownItem className="noti-title" header tag="div">
-											<h6 className="text-overflow m-0">Welcome!</h6>
-										</DropdownItem>
-										<DropdownItem to="/admin/user-profile" tag={Link}>
-											<i className="ni ni-single-02" />
-											<span>My profile</span>
-										</DropdownItem>
-										<DropdownItem to="/admin/user-profile" tag={Link}>
-											<i className="ni ni-settings-gear-65" />
-											<span>Settings</span>
-										</DropdownItem>
-										<DropdownItem to="/admin/user-profile" tag={Link}>
-											<i className="ni ni-calendar-grid-58" />
-											<span>Activity</span>
-										</DropdownItem>
-										<DropdownItem to="/admin/user-profile" tag={Link}>
-											<i className="ni ni-support-16" />
-											<span>Support</span>
-										</DropdownItem>
-										<DropdownItem divider />
-										<DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
-											<i className="ni ni-user-run" />
-											<span>Logout</span>
-										</DropdownItem>
-									</DropdownMenu>
-							</UncontrolledDropdown>
-						</Nav>
-					</Container>
-				</Navbar>
-			</>
-		);
+function AdminNavbar(props) {
+
+	const handleLogout = async () => {
+		const confirm = await window.confirm("Are you sure to logout?")
+
+		if(confirm){
+			localStorage.clear()
+
+			props.history.push('/auth')
+		}
 	}
+
+	return (
+		<>
+			<Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+				<Container fluid>
+					<Link className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" to="/">{props.navTitle}</Link>
+					<Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
+						<FormGroup className="mb-0">
+							<InputGroup className="input-group-alternative">
+								<InputGroupAddon addonType="prepend">
+									<InputGroupText>
+										<i className="fas fa-search" />
+									</InputGroupText>
+								</InputGroupAddon>
+								<Input placeholder="Search" type="text" />
+							</InputGroup>
+						</FormGroup>
+					</Form>
+					<Nav className="align-items-center d-none d-md-flex" navbar>
+						<UncontrolledDropdown nav>
+							<DropdownToggle className="pr-0" nav>
+								<Media className="align-items-center">
+									<span className="avatar avatar-sm rounded-circle">
+										<img alt="..." src={props.imgUser}/>
+									</span>
+									<Media className="ml-2 d-none d-lg-block">
+										<span className="mb-0 text-sm font-weight-bold">{props.nameUser}</span>
+									</Media>
+								</Media>
+							</DropdownToggle>
+								<DropdownMenu className="dropdown-menu-arrow" right>
+									<DropdownItem className="noti-title" header tag="div">
+										<h6 className="text-overflow m-0">Welcome!</h6>
+									</DropdownItem>
+									<DropdownItem to="/admin/user-profile" tag={Link}>
+										<i className="ni ni-single-02" />
+										<span>My profile</span>
+									</DropdownItem>
+									<DropdownItem to="/admin/user-profile" tag={Link}>
+										<i className="ni ni-settings-gear-65" />
+										<span>Settings</span>
+									</DropdownItem>
+									<DropdownItem to="/admin/user-profile" tag={Link}>
+										<i className="ni ni-calendar-grid-58" />
+										<span>Activity</span>
+									</DropdownItem>
+									<DropdownItem to="/admin/user-profile" tag={Link}>
+										<i className="ni ni-support-16" />
+										<span>Support</span>
+									</DropdownItem>
+									<DropdownItem divider />
+									<DropdownItem href="#pablo" onClick={handleLogout}>
+										<i className="ni ni-user-run" />
+										<span>Logout</span>
+									</DropdownItem>
+								</DropdownMenu>
+						</UncontrolledDropdown>
+					</Nav>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
 
-export default AdminNavbar;
+export default withRouter(AdminNavbar);
