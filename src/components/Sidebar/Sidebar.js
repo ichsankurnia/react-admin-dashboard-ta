38609b1f8ce9c64df1ad1118a232c8ebb4d7f854
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-08-08 23:04:19 +0700

    remove unused component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -252,10 +252,11 @@ class Sidebar extends React.Component {
 						
 						{/* Divider */}
 						<hr className="my-3" />
+					
 						{/* Heading */}
-						<h6 className="navbar-heading text-muted">Documentation</h6>
+						{/* <h6 className="navbar-heading text-muted">Documentation</h6> */}
 						{/* Navigation */}
-						<Nav className="mb-md-3" navbar>
+						{/* <Nav className="mb-md-3" navbar>
 							<NavItem>
 								<NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview?ref=adr-admin-sidebar">
 									Getting started
@@ -282,7 +283,8 @@ class Sidebar extends React.Component {
 									Upgrade to PRO
 								</NavLink>
 							</NavItem>
-						</Nav>
+						</Nav> */}
+						
 					</Collapse>
 				</Container>
 			</Navbar>
