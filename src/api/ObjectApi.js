ba6dbb2b2a4b89b8a0d8feed8cbd6fe2079f70e7
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-08-10 19:43:41 +0700

    update baseURL

--- src/api/ObjectApi.js
+++ src/api/ObjectApi.js
@@ -1,4 +1,4 @@
-const base_url = "http://192.168.0.16:3000/api"
+const base_url = "http://192.168.1.7:3006/api"
 // const base_url = "https://darkyasha.goes2nobel.com/TA/back-end/api"
 
 export {base_url}
