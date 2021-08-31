389307817711a840498b0ec7fc2aeacd24a765b7
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-08-31 21:46:37 +0700

    update style

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -41,9 +41,9 @@ class Auth extends React.Component {
 			<>
 				<div className="main-content">
 					<AuthNavbar />
-					<div className="header bg-gradient-info py-7 py-lg-8">
+					<div className="header bg-gradient-info py-lg-6">
 						<Container>
-							<div className="header-body text-center mb-7">
+							<div className="header-body text-center mb-6">
 								<Row className="justify-content-center">
 									<Col lg="5" md="6">
 										<h1 className="text-white">Welcome!</h1>
